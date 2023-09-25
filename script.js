5093640ad5c8772398831a9d0e5f07761c4bf263
Macolister <bispo.mac@gmail.com>
2023-09-25 16:18:26 -0400

    up to renderer

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello Three.js')
+//Scene
+const scene = new THREE.Scene()
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1, 1 , 1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+
+//Mesh composed of geometry and material, must be provided in this order
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//Camera provides POV
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height )
+scene.add(camera)
